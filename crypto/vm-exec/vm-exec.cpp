36d1a9519538b2a9c8180d90b6955380e0eb2afb
krigga <>
2022-11-02 15:00:47 +0300

    Added vm-exec binary to run TVM in WASM
    Co-authored-by: Narek Abovyan <xeroxaltox@gmail.com>

---
+++ crypto/vm-exec/vm-exec.cpp
@@ -0,0 +1,31 @@
+#include "vm/cp0.h"
+#include "td/utils/logging.h"
+#include "StringLog.h"
+#include "common.h"
+
+using namespace std::literals::string_literals;
+
+auto memLog = new StringLog();
+
+extern "C" char *vm_exec(int len, char *_data) {
+  // Init logging
+  td::log_interface = memLog;
+  SET_VERBOSITY_LEVEL(verbosity_DEBUG);
+//  SET_VERBOSITY_LEVEL(verbosity_ERROR);
+  memLog->clear();
+
+  std::string config(_data, len);
+
+  auto res = vm_exec_from_config(config, []() -> std::string { return memLog->get_string(); });
+
+  if (res.is_error()) {
+    std::string result;
+    result += "{";
+    result +=  R"("ok": false,)";
+    result +=  R"("error": ")" + escape_json(res.move_as_error().message().str()) + R"(")";
+    result += "}";
+
+    return strdup(result.c_str());
+  }
+  return strdup(res.move_as_ok().c_str());
+}
